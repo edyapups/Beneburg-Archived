0819f4c5af21197a102cc61dc80094d850d32653
Edgar Zhivaev <edyapups@gmail.com>
2022-10-19 23:45:30 +0300

    Frontend init.

---
+++ frontend/src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<main>
+<h1 class="px-4 py-5 my-5 text-center">
+    Тут пока ничего нет
+</h1>
+</main>
---
+++ frontend/src/main.ts
@@ -0,0 +1,8 @@
+import './static/bootstrap/css/bootstrap.min.css';
+import App from './App.svelte'
+
+const app = new App({
+  target: document.getElementById('app')
+})
+
+export default app
