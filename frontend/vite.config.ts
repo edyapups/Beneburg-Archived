fb62ba3b066de8fbcd7860c212385886b83f8a5f
Edgar Zhivaev <edyapups@gmail.com>
2023-01-07 22:07:19 +0300

    Added proxy config for Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
+import {ConfigEnv, defineConfig, UserConfig} from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+export default defineConfig((env: ConfigEnv) => {
+    let config: UserConfig = {
+        plugins: [svelte()],
+    }
+    if (env.command === 'serve') {
+        config.server = {
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:8080',
+                }
+            }
+        }
+    }
 
+    return config
 })
