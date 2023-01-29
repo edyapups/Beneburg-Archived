059ab4b7a94ac0649487c7013c28f27bb32b8d31
Edgar Zhivaev <edyapups@gmail.com>
2023-01-30 01:20:38 +0300

    Updated script and makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 .PHONY: server_local
 server_local:
 	DOCKER_CONTEXT=local ENV=local zsh ./scripts/rebuild_server.sh
+
+server_remote:
+	DOCKER_CONTEXT=remote ENV=prod zsh ./scripts/rebuild_server.sh
--- scripts/rebuild_server.sh
+++ scripts/rebuild_server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose --env-file=".env.${ENV}" up -d --force-recreate --build --no-deps server
+docker compose --env-file=".env.${ENV}" up -d --force-recreate --build --no-deps server
